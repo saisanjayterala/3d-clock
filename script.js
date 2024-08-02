f345d7abade3260bcdd659a25bb4dad4e6ce2d3f
saisanjayterala <saisanjayterala@gmail.com>
2024-08-02 13:36:47 +0000

    version 2

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-function updateClock() {  
+const secondHand = document.querySelector('.second-hand');  
+const minuteHand = document.querySelector('.minute-hand');  
+const hourHand = document.querySelector('.hour-hand');  
+
+function setDate() {  
     const now = new Date();  
-    const hours = now.getHours() % 12;  
-    const minutes = now.getMinutes();  
+
     const seconds = now.getSeconds();  
+    const secondsDegrees = ((seconds / 60) * 360) + 90;  
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;  
 
-    const hourHand = document.querySelector('.hour');  
-    const minuteHand = document.querySelector('.minute');  
-    const secondHand = document.querySelector('.second');  
+    const minutes = now.getMinutes();  
+    const minutesDegrees = ((minutes / 60) * 360) + ((seconds / 60) * 6) + 90;  
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;  
 
-    const hourDeg = (hours + minutes / 60) * 30;  
-    const minuteDeg = (minutes + seconds / 60) * 6;  
-    const secondDeg = seconds * 6;  
-
-    hourHand.style.transform = `rotate(${hourDeg}deg)`;  
-    minuteHand.style.transform = `rotate(${minuteDeg}deg)`;  
-    secondHand.style.transform = `rotate(${secondDeg}deg)`;  
+    const hours = now.getHours();  
+    const hoursDegrees = ((hours / 12) * 360) + ((minutes / 60) * 30) + 90;  
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;  
 }  
 
-setInterval(updateClock, 1000);  
-updateClock();
+setInterval(setDate, 1000);  
+setDate(); 
